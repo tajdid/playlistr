41cda71391d3ec494b32817d884c019b55d89d06
tajdid <tajdidrahman@gmail.com>
2016-03-24 11:54:58 +0000

    Added paperclip image upload

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
+  
+  has_attached_file :image, styles: { medium: "700x700>", thumb: "350x350>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
